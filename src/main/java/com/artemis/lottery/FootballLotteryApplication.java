8854d9f81ed5090c66d0704f141f56e21fffbef5
oldroot <zhengenshen@gmail.com>
2018-05-08 10:16:32 +0800

    first commit

---
+++ src/main/java/com/artemis/lottery/FootballLotteryApplication.java
@@ -0,0 +1,12 @@
+package com.artemis.lottery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballLotteryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballLotteryApplication.class, args);
+    }
+}
